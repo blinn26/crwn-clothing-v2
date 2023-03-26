3bbe197cec526f3307afa1582393d3887041e14f
blinn26 <your-public-benlinn26@gmail.com>
2023-03-26 08:20:24 -0700

    fixed errors

--- src/routes/sign-in.components/sign-in.components.jsx
+++ src/routes/sign-in.components/sign-in.components.jsx
@@ -1,7 +1,6 @@
-import { signInWithGooglePopup} from '../../utils/firebase/firebase.utils';
+import { signInWithGooglePopup } from '../../utils/firebase/firebase.utils';
 
-
-const SignIn () {
+const SignIn = () => {
   const logGoogleUser = async () => {
     const response = await signInWithGooglePopup();
     console.log(response);
